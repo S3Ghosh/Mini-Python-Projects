8f424db098657c43e2c9ada1769b8f47abe7c793
Sarita Ghosh <119723141+S3Ghosh@users.noreply.github.com>
2024-01-15 09:31:06 +0530

    commit successful

--- Email Validation/email.py
+++ Email Validation/email.py
@@ -3,7 +3,7 @@ j,k,d=0,0,0
 if len(email)>=6:
     if email[0].isalpha():
         if ("@" in email) and (email.count("@")==1):
-            if (email[-4]=='.') ^ (email[-3]=="."):
+            if (email[-4]=='.') ^ (email[-3]=="."):             # Using xor operator, if both true or both are false then answer is false. Have not used or operator, if mail has 2 .. then also condition is true
                 for i in email:
                     if i == i.isspace():
                         k=1
@@ -19,10 +19,10 @@ if len(email)>=6:
                 if k==1 or j==1 or d==1:
                     print("wrong email, condition 5 not satisfied.")
                 else:
-                    print("Email is right")         # string iteration 
+                    print("Email is right")         
             else:
-                print("wrong email, condition 4 not satisfied.")                                # Using xor operator, if both true or both are false then answer is false 
-        else:                                                                                   # have not used or operator, if mail has 2 .. then also condition is true
+                print("wrong email, condition 4 not satisfied.")                                
+        else:                                                                                   
             print("wrong email, condition 3 not satisfied.")
     else:
         print ("wrong email, condition 2 not satisfied.")
